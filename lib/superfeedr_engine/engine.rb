718132e71ae8652881c5bb36cd8eeb458a2f9fde
julien <julien.genestoux@gmail.com>
2015-04-21 15:45:24 +0200

    fixing API for retrieval

--- lib/superfeedr_engine/engine.rb
+++ lib/superfeedr_engine/engine.rb
@@ -17,20 +17,20 @@ module SuperfeedrEngine
     extend SuperfeedrAPI
 
     def self.subscribe(object, opts = {})
-    	raise "Missing url method or property on #{object}" unless object.class.method_defined? :url 
-    	raise "Missing id method or property on #{object}" unless object.class.method_defined? :id 
-    	raise "Missing secret method or property #{object}" unless object.class.method_defined? :id 
+      raise "Missing url method or property on #{object}" unless object.class.method_defined? :url 
+      raise "Missing id method or property on #{object}" unless object.class.method_defined? :id 
+      raise "Missing secret method or property #{object}" unless object.class.method_defined? :id 
 
-    	raise "#{object}#url needs to be a URL" unless valid?(object.url) 
-    	raise "#{object}#id needs to not empty" if object.id.to_s.empty?
-    	raise "#{object}#secret needs to not empty" if object.secret.to_s.empty?
+      raise "#{object}#url needs to be a URL" unless valid?(object.url) 
+      raise "#{object}#id needs to not empty" if object.id.to_s.empty?
+      raise "#{object}#secret needs to not empty" if object.secret.to_s.empty?
 
-    	opts.merge!({
-    		secret: object.secret,
-    		format: 'json' # Force the use of the JSON
+      opts.merge!({
+        secret: object.secret,
+        format: 'json' # Force the use of the JSON
        });
 
-    	super(object.url, object.id, opts)
+      super(object.url, object.id, opts)
     end
 
     def self.retrieve(object, opts = {})
@@ -41,7 +41,7 @@ module SuperfeedrEngine
         format: 'json' # Force the use of the JSON
       });
 
-      SuperfeedrAPI.retrieve_by_topic_url(object.url, opts)
+      self.retrieve_by_topic_url(object.url, opts)
     end
 
     def self.unsubscribe(object, opts = {})
