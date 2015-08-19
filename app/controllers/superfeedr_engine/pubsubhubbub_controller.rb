54a1417b901cdc284020665fef837b141e6d68f4
Shawn Dempsey <sndempsey@gmail.com>
2015-08-19 14:52:45 -0700

    move the signature validation to own class

--- app/controllers/superfeedr_engine/pubsubhubbub_controller.rb
+++ app/controllers/superfeedr_engine/pubsubhubbub_controller.rb
@@ -1,48 +1,82 @@
-require 'openssl' 
+require 'openssl'
 require_dependency "superfeedr_engine/application_controller"
 
 module SuperfeedrEngine
   class PubsubhubbubController < ApplicationController
 
     def notify
-      signature = request.headers['HTTP_X_HUB_SIGNATURE']
-      if !signature
-        Rails.logger.error("Missing signature. Ignored payload for #{params[:feed_id]}. Use retrieve if missing.")
-      else
-        algo, hash = signature.split('=')
-        if algo != "sha1"
-          Rails.logger.error("Unknown signature mechanism #{algo}. Ignored paylod for #{params[:feed_id]}. Use retrieve if missing.")
+      validator = SignatureValidator.new(request, params)
+
+      if validator.valid?
+        feed = validator.feed
+
+        if feed.method(:notified).arity == 2
+          feed.notified(feed_params, request.raw_post)
         else
-          feed = feed_klass.find(params[:feed_id])
-          if !feed
-            Rails.logger.error("Unknown notification for #{params[:feed_id]}.")
-          else
-            digest  = OpenSSL::Digest.new(algo)
-            computed = OpenSSL::HMAC.hexdigest(digest, feed.secret, request.raw_post)
-            if computed == hash
-              params.delete("pubsubhubbub")
-              params.delete("feed_id")
-              if !feed_klass.method_defined?(:notified)
-                Rails.logger.error("Please make sure your #{feed_klass} intances have a 'notified' method.")              
-              elsif feed.method(:notified).arity == 2
-                feed.notified(params, request.raw_post) 
-              else
-                feed.notified(params) 
-              end
-            else
-              Rails.logger.error("Non matching signature. Ignored paylod for #{params[:feed_id]}. Use retrieve if missing.")
-            end
-          end
+          feed.notified(feed_params)
         end
+
+      else
+        Rails.logger.error(validator.error)
       end
+
       render :text => "Thanks"
     end
 
     protected
 
-    def feed_klass
-      SuperfeedrEngine::Engine.feed_class.constantize
+    def feed_params
+      params.delete("pubsubhubbub")
+      params.delete("feed_id")
+    end
+  end
+end
+
+class SignatureValidator
+  attr_reader :feed
+
+  def initialize(request, params)
+    @signature = request.headers['HTTP_X_HUB_SIGNATURE']
+    @feed_id = params[:feed_id]
+  end
+
+  def valid?
+    if @signature.blank?
+      @error = "Missing signature. Ignored payload for #{@feed_id}. Use retrieve if missing."
+      return false
     end
 
+    @algo, @hash = @signature.split('=')
+    if @algo != "sha1"
+      @error = "Unknown signature mechanism #{algo}. Ignored paylod for #{@feed_id}. Use retrieve if missing.")
+      return false
+    end
+
+    @feed = feed_klass.find(@feed_id)
+
+    if @feed.blank?
+      @error = "Unknown notification for #{@feed_id}"
+      return false
+    end
+
+    digest = OpenSSL::Digest.new(@algo)
+    computed = OpenSSL::HMAC.hexdigest(digest, @feed.secret, @request.raw_post)
+    if computed != @hash
+      @error = "Non matching signature. Ignored paylod for #{@feed_id}. Use retrieve if missing."
+      return false
+    end
+
+    if !feed_klass.method_defined?(:notified)
+      @error = "Please make sure your #{@feed_id} intances have a 'notified' method."
+      return false
+    end
+
+    return true
+  end
+
+  protected
+
+  def feed_klass
+    SuperfeedrEngine::Engine.feed_class.constantize
   end
 end
