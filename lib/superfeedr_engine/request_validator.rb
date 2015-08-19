e91bf76c841183b0e21247f106b4eb1b2e75cb18
Shawn Dempsey <sndempsey@gmail.com>
2015-08-19 16:09:09 -0700

    fix the logical operator and fix typo

--- lib/superfeedr_engine/request_validator.rb
+++ lib/superfeedr_engine/request_validator.rb
@@ -11,33 +11,33 @@ class RequestValidator
   def valid?
     signature = @request.headers['HTTP_X_HUB_SIGNATURE']
 
-    if signature.blank?
+    unless signature.present?
       @error = "Missing signature. Ignored payload for #{@feed_id}. Use retrieve if missing."
       return false
     end
 
-    @algo, @hash = signature.split('=')
-    if @algo != "sha1"
+    algo, hash = signature.split('=')
+    unless algo == "sha1"
       @error = "Unknown signature mechanism #{algo}. Ignored paylod for #{@feed_id}. Use retrieve if missing."
       return false
     end
 
     @feed = feed_klass.find(@feed_id)
 
-    if @feed.blank?
+    unless @feed.present?
       @error = "Unknown notification for #{@feed_id}"
       return false
     end
 
-    digest = OpenSSL::Digest.new(@algo)
+    digest = OpenSSL::Digest.new(algo)
     computed = OpenSSL::HMAC.hexdigest(digest, @feed.secret, @request.raw_post)
-    if computed != @hash
+    unless computed == hash
       @error = "Non matching signature. Ignored paylod for #{@feed_id}. Use retrieve if missing."
       return false
     end
 
-    if !feed_klass.method_defined?(:notified)
-      @error = "Please make sure your #{@feed_id} intances have a 'notified' method."
+    unless feed_klass.method_defined?(:notified)
+      @error = "Please make sure your #{@feed_id} instances have a 'notified' method."
       return false
     end
 
